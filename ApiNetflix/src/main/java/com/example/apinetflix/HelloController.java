1a8159524ca3ed5ec35159e427908c0de1165e91
gauravjalal08 <gauravjalal56@gmail.com>
2022-10-07 13:11:13 +0530

    first commit

--- ApiNetflix/src/main/java/com/example/apinetflix/HelloController.java
+++ ApiNetflix/src/main/java/com/example/apinetflix/HelloController.java
@@ -1,12 +1,13 @@
 package com.example.apinetflix;
 
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-        private String Sayhello(){
+        private MysqlxDatatypes.Scalar.String Sayhello(){
         return" welcome to the nexflix Api";
     }
 }
